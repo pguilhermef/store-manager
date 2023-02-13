d4b25e9107f34b5fbf7f190296a0eb8a1de50dc2
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-13 18:15:03 -0300

    refactor: Adiciona 'Model' ao final do nome de cada funçãoem ProductsModels

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -1,12 +1,12 @@
 const connection = require('./connection');
 
-const getAllProducts = async () => {
+const getAllProductsModel = async () => {
   const query = 'SELECT * FROM StoreManager.products';
   const allProducts = await connection.execute(query);
   return allProducts;
 };
 
-const getProductById = async (productId) => {
+const getProductByIdModel = async (productId) => {
   const query = 'SELECT * PRODUCT WHERE id = ?';
   const productFinded = await connection.execute(query, [productId]);
   return productFinded;
