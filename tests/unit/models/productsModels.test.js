705b22921ec1349458c5dfb0d7a00d077a83a10f
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-16 18:46:49 -0300

    test: Cria caso de teste para a camada Model de products

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -1,5 +1,25 @@
-// const {allProductsResponse} = require('../../../__tests__/_dataMock')
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { productsModels } = require('../../../src/models/index')
 
-// describe('Testa a model de products', {
-//   sinon.stub
-// });
+const connection = require('../../../src/models/connection');
+const { allProductsResponse } = require('../../../__tests__/_dataMock')
+
+
+describe('Testa a model de products', () => {
+  it('Lista todas os produtos corretamente', async () => {
+    sinon.stub(connection, 'execute').resolves([allProductsResponse]);
+    const result = await productsModels.getAllProductsModel()
+    expect(result).to.be.deep.equal(allProductsResponse)
+  });
+
+  it('Lista produtos buscados por ID corretamente', async () => {
+    sinon.stub(connection, 'execute').resolves([[allProductsResponse[0]]])
+    const result = await productsModels.getProductByIdModel(1);
+    expect(result).to.be.deep.equal(allProductsResponse[0])
+  })
+
+  afterEach(() => {
+    sinon.restore();
+  })
+});
