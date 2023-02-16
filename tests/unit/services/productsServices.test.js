53a1568bf51d8b4092080005a8998aefb67a8fe5
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-16 19:17:56 -0300

    test: Cria casos de teste para a camada Service

---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { productsServices } = require('../../../src/services/index')
+const { productsModels } = require('../../../src/models/index')
+
+const { allProductsResponse } = require('../../../__tests__/_dataMock')
+
+
+describe('Testa o service de products', () => {
+  it('Lista todas os produtos corretamente', async () => {
+    sinon.stub(productsModels, 'getAllProductsModel').resolves(allProductsResponse);
+    const result = await productsServices.getAllProductsService();
+    expect(result).to.be.deep.equal(allProductsResponse)
+  })
+
+  it('Lista produtos buscados por ID corretamente', async () => {
+    sinon.stub(productsModels, 'getProductByIdModel').resolves(allProductsResponse[0])
+    const result = productsServices.getProductByIdService(1)
+  });
+
+  it('Retorna um 404 caso não exista um produto com o ID buscado', async () => {});
+
+  afterEach(() => {
+    sinon.restore();
+  })
+});
