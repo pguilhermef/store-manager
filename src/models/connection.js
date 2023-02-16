26d9e71e2538a78b95252ac941ace607bd78ac85
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-16 17:31:14 -0300

    feat: Faz a requisição do dotenv direto no server

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
+  host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  // port: process.env.MYSQL_PORT,
+  // database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
