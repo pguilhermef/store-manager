4b0d3925ed2dbb85b2bae9b3cded4f6f90c0c64e
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-16 17:31:53 -0300

    feat: Desestrutura o resultado de allProducts ao buscar todos os produtos

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -2,13 +2,13 @@ const connection = require('./connection');
 
 const getAllProductsModel = async () => {
   const query = 'SELECT * FROM StoreManager.products';
-  const allProducts = await connection.execute(query);
+  const [allProducts] = await connection.execute(query);
   return allProducts;
 };
 
 const getProductByIdModel = async (productId) => {
   const query = 'SELECT * PRODUCT WHERE id = ? ORDER BY id ASC';
-  const productFinded = await connection.execute(query, [productId]);
+  const [[productFinded]] = await connection.execute(query, [productId]);
   return productFinded;
 };
 
