312acbbc4fc8aef1987a829e2fa2b0bed6b827d2
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-20 15:59:39 -0300

    refactor: Agora todos os retornos de Service tem retorno de objeto

--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,20 +1,30 @@
 const productsModels = require('../models/productsModels');
+const schema = require('./validations/validationsInputValues');
 
 const getAllProductsService = async () => {
   const allProducts = await productsModels.getAllProductsModel();
-  return allProducts;
+  return { type: null, message: allProducts };
 };
 
 const getProductByIdService = async (productId) => {
+  const error = schema.validateId(productId);
+  if (error.type) return error.message;
+
   const productFinded = await productsModels.getProductByIdModel(productId);
-  return productFinded;
+  if (!productFinded) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+
+  return { type: null, message: productFinded };
 };
 
 const createNewProductService = async (name) => {
+  const error = schema.validateProductName(name);
+  if (error.type) return error.message;
+
+  // cria o produto por de trás dos panos e retorna a busca por Id do produto buscado
   const newProductId = await productsModels.createNewProductModel({ name });
   const createdProduct = await productsModels.getProductByIdModel(newProductId);
   
-  return createdProduct;
+  return { type: null, message: createdProduct };
 };
 
 module.exports = {
