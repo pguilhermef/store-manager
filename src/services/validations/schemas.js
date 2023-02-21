7b3844745a189fa83870e121bbd11273ea4267d8
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-21 19:51:17 -0300

    feat: Cria schema de validação para Sales

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -2,12 +2,15 @@ const Joi = require('joi');
 
 const idSchema = Joi.number().integer().min(1).required();
 
-const nameSchema = Joi.string().min(5).required().messages({
-  'any.required': '"name" is required',
-  'string.min': '"name" length must be at least 5 characters long',
+const nameSchema = Joi.string().min(5).required();
+
+const saleSchema = Joi.object().keys({
+  productId: Joi.number().min(1).required(),
+  quantity: Joi.number().min(1).required(),
 });
 
 module.exports = {
   idSchema,
   nameSchema,
+  saleSchema,
 };
