d548a8f1d27356a418f35eef92de3a096e99dda2
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-21 18:28:41 -0300

    test: Organiza os testes de ModelProducts em novos describes

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -6,24 +6,29 @@ const connection = require('../../../src/models/connection');
 const { allProductsResponse } = require('../../../__tests__/_dataMock')
 
 
-describe('Testa a model de products', () => {
-  it('Lista todas os produtos corretamente', async () => {
-    sinon.stub(connection, 'execute').resolves([allProductsResponse]);
-    const result = await productsModels.getAllProductsModel()
-    expect(result).to.be.deep.equal(allProductsResponse)
+describe('Model de Products', () => {
+  describe('Listagem de produtos', () => {
+    it('Lista todas os produtos corretamente', async () => {
+      sinon.stub(connection, 'execute').resolves([allProductsResponse]);
+      const result = await productsModels.getAllProductsModel()
+      expect(result).to.be.deep.equal(allProductsResponse)
+    });
+  
+    it('Lista produtos buscados por ID corretamente', async () => {
+      sinon.stub(connection, 'execute').resolves([[allProductsResponse[0]]])
+      const result = await productsModels.getProductByIdModel(1);
+      expect(result).to.be.deep.equal(allProductsResponse[0])
+    })
   });
 
-  it('Lista produtos buscados por ID corretamente', async () => {
-    sinon.stub(connection, 'execute').resolves([[allProductsResponse[0]]])
-    const result = await productsModels.getProductByIdModel(1);
-    expect(result).to.be.deep.equal(allProductsResponse[0])
-  })
-
-  it('Cria um novo produto com sucesso', async () => {
-    sinon.stub(connection, 'execute').resolves([{ insertId: 42 }])
-    const result = await productsModels.createNewProductModel('Escudo do Capitão América')
-    expect(result).to.be.deep.equal(42)
+  describe('Criação de produtos', () => {
+    it('Cria um novo produto com sucesso', async () => {
+      sinon.stub(connection, 'execute').resolves([{ insertId: 42 }])
+      const result = await productsModels.createNewProductModel('Action figure Luffy')
+      expect(result).to.be.deep.equal(42)
+    });
   });
+
 
   afterEach(() => {
     sinon.restore();
