b3a747f2ab22b21f34b1804247b0f5429d47248b
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-22 14:32:31 -0300

    revert: Coloca novamente mensagens personalizadas para o nameSchema

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -2,15 +2,21 @@ const Joi = require('joi');
 
 const idSchema = Joi.number().integer().min(1).required();
 
-const nameSchema = Joi.string().min(5).required();
+const nameSchema = Joi.string().min(5).required().messages({
+  'any.required': '"name" is required',
+  'string.min': '"name" length must be at least 5 characters long',
+});
 
 const saleSchema = Joi.object().keys({
   productId: Joi.number().min(1).required(),
   quantity: Joi.number().min(1).required(),
 });
 
+const saleArraySchema = Joi.array().items(saleSchema);
+
 module.exports = {
   idSchema,
   nameSchema,
   saleSchema,
+  saleArraySchema,
 };
