fcd42fe19c782380feef6bce3900453755b061f4
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-22 13:43:31 -0300

    refactor: Coloca colunas da tabela direto no salesModel, sem dinamismo

--- src/models/salesModels.js
+++ src/models/salesModels.js
@@ -7,7 +7,8 @@ const getAllSalesModel = async () => {
 };
 
 const getSaleByIdModel = async (saleId) => {
-  const query = 'SELECT * FROM StoreManager.sales WHERE id = ? ORDER BY saleId, productId ASC';
+  const query = `SELECT * FROM StoreManager.sales_products
+  WHERE sale_id = ? ORDER BY sale_id, product_id ASC`;
   const [[saleFinded]] = await connection.execute(query, [saleId]);
   return saleFinded;
 };
@@ -18,18 +19,17 @@ const createNewSaleDateModel = async () => {
   return insertId;
 };
 
-const createNewSaleDetailsModel = async (sales) => {
-  const colums = Object.keys(sales).join(', ');
-
-  const placeholders = Object.keys(sales)
+const createNewSaleDetailsModel = async (sale) => {
+  const placeholders = Object.keys(sale)
     .map((_key) => '?')
     .join(', ');
   
-  const query = `INSERT INTO StoreManager.sales_products (${colums}) VALUES (${placeholders})`;
+  const query = `INSERT INTO StoreManager.sales_products
+  (sale_id, product_id, quantity) VALUES (${placeholders})`;
 
-  const itemsSold = await connection.execute(query, [...Object.values(sales)]);
+  const [{ insertId }] = await connection.execute(query, [...Object.values(sale)]);
   
-  return itemsSold;
+  return insertId;
 };
 
 module.exports = {
