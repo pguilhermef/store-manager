b0c97896d392103c6de984b3afa8619cde4b7c75
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-23 17:32:53 -0300

    feat: Adiciona o "numberMin" a lista do mapError

--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -3,6 +3,7 @@ const errorMap = {
   IS_REQUIRED: 400,
   'any.required': 400,
   'string.min': 422,
+  'number.min': 422,
   // NOT FOUND
   PRODUCT_NOT_FOUND: 404,
   // GREATER OR EQUAL || GENERAL INVALID VALUES
