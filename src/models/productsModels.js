bee5cf43765505c9f1972bf912d42a361e214c68
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-24 18:43:25 -0300

    feat: Cria model para update de produto

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -26,8 +26,17 @@ const createNewProductModel = async (product) => {
   return insertId;
 };
 
+const updateProductModel = async (name, id) => {
+  const [result] = await connection.execute(
+    'UPDATE StoreManager.products SET name = ? WHERE id = ?',
+    [name, id],
+  );
+  return result;
+};
+
 module.exports = {
   getAllProductsModel,
   getProductByIdModel,
   createNewProductModel,
+  updateProductModel,
 };
